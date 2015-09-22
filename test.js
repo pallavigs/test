4a4035eafcb65c97ec7446f46cf6656ecaa479aa
pallavigs <pallavi.gs@gmail.com>
2015-09-22 11:40:56 +0530

    Update test.js

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ describe('User visits Hello World page', function() {
   const browser = new Browser();
 
   before(function(done) {
-    browser.visit('http://<your-ip>:8081/<your-path>/', done);
+    browser.visit('http://23.251.139.5:8081/helloworld/', done);
   });
 
   describe('submits form', function() {
